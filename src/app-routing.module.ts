3f88ac2723c2bd2ca6ddb6520419efae0e3ba12b
Alexsandra Alvarez Vega <alexsandra.alvarez@capgemini.com>
2021-08-10 14:06:26 -0500

    Cambios en appRouting, se agrego el path para Login

--- src/app-routing.module.ts
+++ src/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './app/register/register.component';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './app/login/login.component';
 
 const routes: Routes = [
-  {path:'register', component: RegisterComponent}
+  {path:'register', component: RegisterComponent},
+  {path: 'login', component:LoginComponent},
 
 ]
 
